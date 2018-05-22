5415e77bb53cbe1ffa736d3b81b7cfc2191c991e
David Brownell <david.brownell@microsoft.com>
2018-05-22 11:17:12 -0700

    Added generated cmake files

---
+++ bindings/csharp/CNTKLibraryManagedDll/build/Linux/Debug/CSharpBindings/CMakeFiles/CSharpBindings.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/local/bindings/csharp/Swig/Generated/cntk_cs_wrap.h"
+  "/local/bindings/csharp/Swig/Generated/cntk_cs_wrap.cxx"
+  "CMakeFiles/CSharpBindings.dir/Generated/cntk_cs_wrap.cxx.o"
+  "Cntk.Core.CSBinding-2.5.1d.pdb"
+  "Cntk.Core.CSBinding-2.5.1d.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSharpBindings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bindings/csharp/CNTKLibraryManagedDll/build/Linux/Release/CMakeFiles/CNTKLibraryManagedDll.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CNTKLibraryManagedDll"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/CNTKLibraryManagedDll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

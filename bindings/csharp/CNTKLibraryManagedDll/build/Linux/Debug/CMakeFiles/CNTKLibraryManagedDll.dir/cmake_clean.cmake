bd5cc7cf4a4c54e956287dcf58ff21899ce43697
David Brownell <david.brownell@microsoft.com>
2018-05-22 14:30:49 -0700

    Removed checkin of generated build files

--- bindings/csharp/CNTKLibraryManagedDll/build/Linux/Debug/CMakeFiles/CNTKLibraryManagedDll.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CNTKLibraryManagedDll"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CNTKLibraryManagedDll.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
